69c9ba659d8144cb1bb3e91712ead216458eef77
Giacomo Lawrance <giacomolawrance@gmail.com>
2019-03-17 17:41:32 +0000

    added working coming soon page

--- js/countdown.js
+++ js/countdown.js
@@ -1,22 +1,15 @@
-// Date counting down to
-var countDownDate = new Date("Jan 5, 2018 15:37:25").getTime();
-
+var countDownDate = new Date("Jan 1, 2021 12:00:00").getTime();
 var x = setInterval(function() {
-
-  var now = new Date().getTime();
-
-  var distance = countDownDate - now;
-
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
+	var now = new Date().getTime();
+	var distance = countDownDate - now;
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+		+ minutes + "m " + seconds + "s ";
+	if (distance < 0) {
+		clearInterval(x);
+		document.getElementById("demo").innerHTML = "Coming soon!";
+	}
 }, 1000);
